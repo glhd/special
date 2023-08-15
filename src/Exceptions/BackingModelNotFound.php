30581ae7489c8f7e687f6ba215e33dfecdcdc8e3
Chris Morrell <inxilpro@users.noreply.github.com>
2023-08-15 10:50:38 -0400

    BackingModelNotFound should not trigger 404

--- src/Exceptions/BackingModelNotFound.php
+++ src/Exceptions/BackingModelNotFound.php
@@ -3,15 +3,15 @@
 namespace Glhd\Special\Exceptions;
 
 use BackedEnum;
-use Illuminate\Database\RecordsNotFoundException;
+use RuntimeException;
 
-class BackingModelNotFound extends RecordsNotFoundException
+class BackingModelNotFound extends RuntimeException
 {
 	public function __construct(
 		public BackedEnum $special_enum
 	) {
 		$basename = class_basename($this->special_enum);
 		
-		parent::__construct("Unable to find model for '{$basename}::{$this->special_enum->name}'");
+		parent::__construct("Unable to find backing model for '{$basename}::{$this->special_enum->name}'");
 	}
 }
